a0b73d948a29118823faa731ec27f5b8100383a4
Mel Yasuda and Victoria Oh <victoria@>
2018-08-07 16:47:40 -0700

    add words and definition

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+  $("#js").click(function(){
+    $("#def1").toggle();
+  });
+
+  $("#operators").click(function(){
+    $("#def2").toggle();
+  });
+
+  $("#variables").click(function(){
+    $("#def3").toggle();
+  });
+
+  $("#vnc").click(function(){
+    $("#def4").toggle();
+  });
+
+  $("#functions").click(function(){
+    $("#def5").toggle();
+  });
+
+  $("#methods").click(function(){
+    $("#def6").toggle();
+  });
+
+  $("#arguments").click(function(){
+    $("#def7").toggle();
+  });
+
+  $("#parameters").click(function(){
+    $("#def8").toggle();
+  });
+
+  $("#return").click(function(){
+    $("#def9").toggle();
+  });
+
+  $("#cm").click(function(){
+    $("#def10").toggle();
+  });
+
+  $("#strings").click(function(){
+    $("#def11").toggle();
+  });
+
+  $("#booleans").click(function(){
+    $("#def12").toggle();
+  });
+
+  $("#undefined").click(function(){
+    $("#def13").toggle();
+  });
+
+  $("#nan").click(function(){
+    $("#def14").toggle();
+  });
+
+  $("#escape").click(function(){
+    $("#def15").toggle();
+  });
+
+  $("#alert").click(function(){
+    $("#def16").toggle();
+  });
+
+  $("#comments").click(function(){
+    $("#def17").toggle();
+  });
+
+  $("#jquery").click(function(){
+    $("#def18").toggle();
+  });
+
+  $("#attributes").click(function(){
+    $("#def19").toggle();
+  });
+});
